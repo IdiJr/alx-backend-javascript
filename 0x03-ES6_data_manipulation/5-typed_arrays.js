1935aac2f2782770b17c39e311b27241cf74ba2b
IdiJr <idialijnr@gmail.com>
2023-12-05 18:05:33 +0000

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  
+  try {
+    view.set([value], position);
+  } catch (e) {
+    throw Error ('Position outside range');
+  }
+  return new DataView(buffer);
+}
