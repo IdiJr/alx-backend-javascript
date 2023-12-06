e0df18e43b2aa805be3e03d0d47cc3083977a80f
IdiJr <idialijnr@gmail.com>
2023-12-06 23:02:34 +0000

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,16 @@ interface Teacher {
   interface Directors extends Teacher {
     numberOfReports: number;
   }
+
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const firstLetter = firstName.charAt(0).toUpperCase();
+    const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  
+    return `${firstLetter}. ${fullLastName}`;
+  };
+
+  export { printTeacher};
